9b4344bfb38df061fd2188e94995d39185b5414e
OdashevBexruzbek <bexruzbekodashev@gmail.com>
2024-07-05 13:54:31 +0500

    todoApp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 // import { Home, About, Contact } from "../pages";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { auth } from "../firebase/firebaseConfig";
+import { signOut } from "firebase/auth";
 
 function Navbar() {
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("You LogOut website");
+      })
+      .catch((error) => {
+        toast.error("Failed to log out")
+      });
+  };
+
+  const { user } = useSelector((state) => state.user);
+
   return (
     <div className="bg-base-300 mb-3">
       <div className="navbar site-container">
@@ -11,27 +27,48 @@ function Navbar() {
             TodoApp
           </Link>
         </div>
-        <ul className="menu menu-horizontal gap-3">
-          <li>
-            <NavLink
-              className="btn btn-secondary"
-              activeClassName="btn-primary"
-              exact
-              to="/"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="btn btn-secondary"
-              activeClassName="btn-primary"
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
+        <div className="navabr-center">
+          <ul className="menu menu-horizontal gap-3">
+            <li>
+              <NavLink
+                className="btn btn-secondary"
+                activeClassName="btn-primary"
+                exact
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="btn btn-secondary"
+                activeClassName="btn-primary"
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          {user && (
+            <div className="flex items-center justify-between mr-3 bg-slate-500  p-2 rounded-3xl cursor-grab">
+              {" "}
+              <p className="mr-3 font-semibold text-xl text-gray-800 dark:text-gray-300">
+                {user.displayName}
+              </p>
+              <div className="avatar">
+                <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                  {user && <img src={user.photoURL} alt="user profile photo" />}
+                </div>
+              </div>
+              <button onClick={logOut} className="btn btn-secondary ml-20">
+                Log out
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='site-container'>
+      <h2>Siz bizning saytda ko'plab qulayliklarga ega bo'lishingiz mumkin. Masalan: Tasklarni qo'shish; tasklarni tahrirlash; tasklarni o'chirish; bajarilgan vaqtini ko'rish.</h2>
+    </div>
   )
 }
 
